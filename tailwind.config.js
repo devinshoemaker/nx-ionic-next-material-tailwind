afffe029cee75d84be2f611a5b60e5177c9e9327
Devin Shoemaker <devinshoe@gmail.com>
2021-01-30 14:38:21 -0600

    Initialize Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['apps/mobile/**/*.tsx', 'apps/web/**/*.tsx'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
